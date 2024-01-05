21b0847bf830fc1e474cb76760a90e1bcba52b75
shane <shanepark.dev@gmail.com>
2024-01-05 17:55:16 +0900

    feat: add mdblog for linux OS
    my ubuntu has bit difference shell grammar from macOS

--- mdblog-linux.sh
+++ mdblog-linux.sh
@@ -7,15 +7,11 @@ markdown_file="$1"
 
 # Function to replace local file paths with GitHub URLs
 replace_paths_with_urls() {
-  local_path="/Users/shane/Documents/GitHub/mdblog/"
+  linux_path="/home/shane/Documents/git/shane/mdblog/"
   github_url="https://raw.githubusercontent.com/ShanePark/mdblog/main/"
 
   # Replace local file paths with GitHub URLs
-  if [[ "$OSTYPE" == "darwin"* ]]; then
-    sed -i '' -E "s!${local_path}!${github_url}!g" "$markdown_file"
-  else
-    sed -i -E "s!${local_path}!${github_url}!g" "$markdown_file"
-  fi
+  sed -i -E "s!${linux_path}!${github_url}!g" "$markdown_file"
   echo "[Completed] Local file paths replaced with GitHub URLs."
 }
 
