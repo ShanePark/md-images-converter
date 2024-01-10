deaf211cc1234a47899b7abbb32ed7ad6b105ff0
shane <shanepark.dev@gmail.com>
2024-01-10 15:13:58 +0900

    refactor: makes it work both on mac & linux

--- mdblog.sh
+++ mdblog.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 if [ $# -eq 0 ]; then
   echo "Please provide a markdown file path as an argument."
   exit 1
@@ -5,16 +7,24 @@ fi
 
 markdown_file="$1"
 
+run_sed() {
+  local pattern=$1
+  local sed_option='-i -E'
+  [[ "$OSTYPE" == "darwin"* ]] && sed_option='-i '' -E'
+  sed $sed_option "$pattern" "$markdown_file"
+}
+
 # Function to replace local file paths with GitHub URLs
 replace_paths_with_urls() {
-  local_path="/Users/shane/Documents/GitHub/mdblog/"
+  macos_path="/Users/shane/Documents/GitHub/mdblog/"
+  linux_path="/home/shane/Documents/git/shane/mdblog/"
   github_url="https://raw.githubusercontent.com/ShanePark/mdblog/main/"
 
   # Replace local file paths with GitHub URLs
   if [[ "$OSTYPE" == "darwin"* ]]; then
-    sed -i '' -E "s!${local_path}!${github_url}!g" "$markdown_file"
+    run_sed "s!${macos_path}!${github_url}!g"
   else
-    sed -i -E "s!${local_path}!${github_url}!g" "$markdown_file"
+    run_sed "s!${linux_path}!${github_url}!g"
   fi
   echo "[Completed] Local file paths replaced with GitHub URLs."
 }
